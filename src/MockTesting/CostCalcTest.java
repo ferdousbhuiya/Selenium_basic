faf766a5ddf5b726baa712de61982db4bdb7c505
ferdousbhuiya <ferdousbhuiya@gmail.com>
2022-10-21 00:24:16 -0400

    TDD Lab1

---
+++ src/MockTesting/CostCalcTest.java
@@ -0,0 +1,59 @@
+package MockTesting;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CostCalcTest {
+private static Student stu1;
+private static CostCalc calc;
+
+@BeforeAll
+    static void setUp()
+{
+    stu1 = new Student("George Frank", 24,3, true, 4);
+    calc = new CostCalc();
+
+}
+@Test
+    void calcCostTest()
+{
+    int expected = 550;
+    int actual = calc.calcCost(stu1);
+    //assertThat(calc.calcCost(stu1),is(550));
+    assertEquals(expected, actual);
+}
+@Test
+    void weeksCostTest()
+{
+    int expected = 2000;
+    int actual = calc.weeksCost(12);
+    assertEquals(expected, actual);
+}
+@Test
+    void residentTest()
+{
+    int expected = 70;
+    int actual = calc.residentCost(700,false);
+    assertEquals(expected, actual);
+}
+    @Test
+    void yearsDisTest()
+    {
+        int expected = 150;
+        int actual = calc.yearsDiscount(3);
+        assertEquals(expected, actual);
+    }
+
+}
+
+
+/*
+
+TDD
+Explaining TDD
+https://juristr.com/blog/2010/10/test-driven-development-and-automated/
+https://martinfowler.com/bliki/TestDrivenDevelopment.html
+https://www.barbarianmeetscoding.com/blog/learning-test-driven-development-with-kent-beck
+ */
--- src/MockTesting/IFeeProvider.java
+++
@@ -1,7 +0,0 @@
-package MockTesting;
-
-public interface IFeeProvider {
-    public void setFee(double fee);
-    public double feesDue();
-    public void payFee(double fee);
-}
